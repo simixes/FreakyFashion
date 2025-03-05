c5396d2e04e5185c449decdf69e4c55378d7d7fd
Simon Jacobson <simon.jacobson@hotmail.com>
2025-03-05 13:42:23 +0100

    Basket html & css done

--- client/src/pages/Basket/Basket.jsx
+++ client/src/pages/Basket/Basket.jsx
@@ -7,7 +7,8 @@ function Basket() {
   
   const testProduct = [
     { id: 1, itemName: "Jeans", price: 399 },
-    { id: 2, itemName: "Jacket", price: 699 }
+    { id: 2, itemName: "Jacket", price: 699 },
+    { id: 3, itemName: "Trousers", price: 199 }
   ];
 
 
@@ -41,7 +42,7 @@ function Basket() {
 <main>
   <section className="basket-section-mobile">
     <div>
-      <h1>Varukorgen</h1>
+    <h1>Varukorgen</h1>
     </div>
     <div className="basket-item-container-m">
       {testProduct.map(product => (
@@ -74,9 +75,60 @@ function Basket() {
     </div>
   </section>
 
-  <section className='basket-section-a640'>
-    
-  </section>
+  <section className="basket-section-a640">
+  <div>
+    <h1>Varukorgen</h1>
+  </div>
+  <div>
+    <table className="basket-table">
+      <thead>
+        <tr>
+          <th>Produkt</th>
+          <th>Antal</th>
+          <th>Pris</th>
+          <th>Totalt</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {testProduct.map(product => (
+          <tr key={product.id}>
+            <td>{product.itemName}</td>
+            <td>{quantities[product.id]}</td>
+            <td>{product.price} kr</td>
+            <td>{quantities[product.id] * product.price} kr</td>
+            <td>
+              <input 
+                className="basket-input"
+                type="number"
+                value={quantities[product.id]}
+                min="1"
+                onChange={(e) => updateQuantity(product.id, parseInt(e.target.value))}
+              />
+              <img 
+                className="trashcan-img"
+                src="/images/trashcan-img-svg.svg" 
+                alt="Delete from basket" 
+                onClick={() => deleteProductFromBasket(product.id)} 
+              />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    <div className='basket-total-amount'>
+      <h3>Produkter: {getTotalCount()} st</h3>
+      <h3>Totalt: {getTotalPrice()} SEK</h3>
+    </div>
+  </div>
+  
+  <div className="basket-btn-container">
+    <button className="basket-btn">Till Kassan</button>
+  </div>
+</section>
+
+
+
 </main>
 
   );
